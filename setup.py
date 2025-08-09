1aca0a0f2e883790882e0a00aa6b16b034ade193
Allan Fenelon <allanpereira.fenelon@gmail.com>
2025-08-09 14:59:20 -0300

    feat: implementar validação cruzada adaptativa com heap e priorização dinâmica dos folds

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name="AdaptiveFoldTS",
     version="0.1.0",
-    author="Allan",
+    author="Allan Pereira Fenelon",
     author_email="allanpereira.fenelon@example.com",
     description="Validação Cruzada Adaptativa para Séries Temporais com Priorização Inteligente de Folds",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/allanfenelon/AdaptiveFoldTS",  # alterar para seu repositório
-    packages=find_packages(),
+    url="https://github.com/allanfenelon/AdaptiveFoldTS",
+    package_dir={"": "src"},             # Código fonte está dentro de src/
+    packages=find_packages(where="src"), # Procura pacotes dentro de src/
     python_requires=">=3.7",
     install_requires=[
         "numpy",
